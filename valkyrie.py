50d6e12f479e6d60fbe6f0bfe0d54e7199d9d706
ipzero209 <npl127.0.0.1@gmail.com>
2018-05-25 13:36:28 -0700

    Error fix - Debug build (no confirmation on uninstall)
    Signed-off-by: ipzero209 <npl127.0.0.1@gmail.com>

--- valkyrie.py
+++ valkyrie.py
@@ -6,7 +6,7 @@ import xml.etree.ElementTree as et
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 import logging
-from logging import handlers
+from logging.handlers
 import os
 import socket
 import shelve
